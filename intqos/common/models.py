46f8355c7d9fa480b94069211021fd1475d54ab9
charefdz1995 <ccherrad@gmail.com>
2019-05-23 19:16:34 +0100

    network base database

--- intqos/common/models.py
+++ intqos/common/models.py
@@ -1,3 +1,35 @@
-from django.db import models
+from mongoengine import * 
 
-# Create your models here.
+
+
+class interface(EmbededDocument):
+	interface_type = StringField(required=True)
+	interface_shelf = intField(required=False)
+	interface_slot = intField(required=True)
+	interface_port = intField(required=True)
+	interface_name = StringField(required=True)
+
+class access(EmbededDocument):
+	management_address = StringField(required=True)
+	username = StringField(required=True)
+	password = StringField(required=True)
+	enable_secret = StringField(required=True)
+
+class switch(EmbededDocument):
+	hostname = StringField(required=True)
+	management = EmbededDocumentField(access)
+	interfaces = listField(EmbededDocumentField(interface))
+
+class link_side(EmbededDocument):
+	switch = ReferenceField(switch)
+	interface = ReferenceField(interface)
+
+class link(EmbededDocument):
+	link_description = StringField(required=False)
+	from_interface =  EmbededDocumentField(link_side)
+	to_interface = EmbededDocumentField(link_side)
+
+class topology(Document):
+	topology_name = StringField(required=True)
+	switches = listField(EmbededDocumentField(switch))
+	links = listField(EmbededDocumentField(link))
