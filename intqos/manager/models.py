7ad6fd3a4d425b79742e290a23969ca0f075ccf9
DJchafie <36542051+DJchafie@users.noreply.github.com>
2019-05-27 16:38:46 +0100

    Update models.py

--- intqos/manager/models.py
+++ intqos/manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from mongoengine import * 
+from common.models import * 
+zone_type = (('Egress', 'Egress'),('Ingress', 'Ingress'))
+
+class interface(interface_base):
+	zone_type = StringField(max_length=6, choices=zone_type)
+class switch(switch):
+	zone_type = StringField(max_length=6, choices=zone_type)
+
