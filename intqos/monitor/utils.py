58d13e417f362aa3a5d45b2f7243a561097d5b64
charefdz1995 <ccherrad@gmail.com>
2019-06-02 17:42:36 +0100

    Adding utils.py :--> netflow parser

---
+++ intqos/monitor/utils.py
@@ -0,0 +1,21 @@
+from .models import * 
+
+
+def packet_parser(self):
+	flow_instance = flow()
+	flow_instance.ipv4_src_addr = self[]
+	flow_instance.ipv4_dst_addr = self[]
+	flow_instance.ipv4_protocol = self[]
+	flow_instance.transport_src_port = self[]
+	flow_instance.transport_dst_port = self[]
+	flow_instance.type_of_service = self[]
+	flow_instance.application_name = self[]
+	flow_instance.counter_bytes = self[]
+	flow_instance.counter_pkts = self[]
+	flow_instance.first_switched = self[]
+	flow_instance.last_switched = self[]
+	flow_instance.collection_time = self[]
+	flow_instance.interface_index = self[]
+	flow_instance.device = self[] # TODO : some work here quering the db searching for who send this packets
+	flow_instance.save()
+
