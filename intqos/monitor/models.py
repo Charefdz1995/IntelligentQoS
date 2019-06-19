ee8d1d1ed594d8be8cd2717a008be06de7e6a2af
charefdz1995 <ccherrad@gmail.com>
2019-06-19 02:41:31 +0100

    using napalm in connecting

--- intqos/monitor/models.py
+++ intqos/monitor/models.py
@@ -3,7 +3,12 @@ from netaddr import *
 from mongoengine import *
 from common.models import *
 from jinja2 import Environment, FileSystemLoader
+<<<<<<< HEAD
+from intqos.intqos.settings import NET_CONF_TEMPLATES
+from napalm import get_network_driver 
+=======
 from intqos.settings import NET_CONF_TEMPLATES
+>>>>>>> 29a5d7329a13873d308b9ee460e1c91e65bca9a4
 
 
 class interface(interface):
@@ -28,56 +33,51 @@ class device(device):
 		for interface in self.interfaces : 
 			interfaces_output += interface.configure_netflow()
 
-		return global_output + interfaces_output
+		config = (global_output + interfaces_output).splitlines()
 
-	def configure_ip_sla(self,operation,dst_ip,dst_port,src_ip,src_port):
+		return self.connect().cli(config)
+
+	def configure_ip_sla(self,operation,record):
 		env = Environment(loader=FileSystemLoader(NET_CONF_TEMPLATES))
 		template = env.get_template("ip_sla.j2")
-		output = template.render(operation = operation,dst_ip =dst_ip,
-								dst_port = dst_port ,src_ip = src_ip ,src_port = src_port)
+		output = template.render(operation = operation,record = record)
+		config = output.splitlines()
+		return self.connect().cli(config)
 
-		return output
+
 	def configure_ip_sla_responder(self):
-		return ['ip sla responder']
+		return self.connect().cli(['ip sla responder'])
+
 
 	def pull_ip_sla_stats(operation,src_device):
 		jitter_cmd = "show ip sla statistics {} | include Destination to Source Jitter".format(str(operation))
 		delay_cmd = "show ip sla statistics {} | include Destination to Source Latency".format(str(operation))
-		pakcet_loss = None
-		# The result are saved here to be parsed
+		config = [jitter_cmd,delay_cmd]
 
-		result_jitter = None
-		result_delay = None
+		result = self.connect().cli(config)
 
-		# after getting the result of this
-		jitter = re.findall("\+d",result_jitter)
-		jitter = int(jitter[1])
-		delay = re.findall("\+d",result_delay)
-		delay = int(delay[1])
-        
-	def push_config(self,config_commands):
-		from netmiko import ConnectHandler 
+		jitter = int(re.findall("\+d",result[jitter_cmd])[1])
+		delay = int(re.findall("\+d",result[delay_cmd])[1])
 
-		device_info = {
-			"device_type" : "cisco_ios", 
-			"ip" : self.management.management_address,
-			"username" : self.management.username, 
-			"password" : self.management.password, 
-		}
+		return jitter, delay 
+s
 
+
+	def connect(self):
+		driver = get_network_driver("ios")
+		device = None
 		try:
-			device = ConnectHandler(**device_info)
-			config_commands = config_commands.splitlines()
-			device.send_config_set(config_commands)
-			device.disconnect()
+			device = driver(self.management.management_address,self.management.username,
+							self.management.password)
+			device.open()
 		except Exception as e:
 			print(e)
+		return device
 
 
 class topology(topology):
 
 	def get_ip_sla_devices(self,record):
-
 		src_ip = IPAddress(record.IPV4.SRC.ADDR) 
 		dst_ip = IPAddress(record.IPV4.DST.ADDR)
 		src_device = None
@@ -91,4 +91,63 @@ class topology(topology):
 					src_device = device
 				if dst_ip in network:
 					dst_device = device
-return src_device,dst_device
+		return src_device,dst_device
+
+
+	def kill_ip_sla(self): #TODO : killing the ip sla of inactive flows after 5 min 
+		pass 
+
+
+
+
+
+
+
+class flow(DynamicDocument):
+	ipv4_src_addr = StringField(required = True)
+	ipv4_dst_addr = StringField(required = True)
+	ipv4_protocol = IntField(required = True)
+	transport_src_port = IntField(required = True)
+	transport_dst_port = IntField(required = True)
+	type_of_service = IntField(required = True)
+	application_name = StringField(required = True)
+	
+	
+
+class netflow_fields(DynamicDocument):
+
+	#Real time information about flow in the monitor. 
+	counter_bytes = IntField(required = True)
+	counter_pkts = IntField(required = True)
+	first_switched = FloatField(required = True)
+	last_switched  = FloatField(required = True)
+	#QoS parameters
+	bandwidth = FloatField(required = False)
+	#=======================================
+	# Device related Information
+	collection_time = StringField(required = True)
+	input_int = IntField(required = True)
+	output_int = IntField(required = True)
+	device = ReferenceField(device)
+	flow = ReferenceField(flow)
+	#=======================================
+
+<<<<<<< HEAD
+class ip_sla(document):
+	operation = SequenceField()
+	device_ref = ReferenceField(device)
+
+=======
+class ip_sla(Document):
+	operation = IntField(required= True)
+>>>>>>> 29a5d7329a13873d308b9ee460e1c91e65bca9a4
+
+
+
+class ip_sla_info(Document):
+	avg_jitter = IntField(required = True)
+	avg_delay = IntField(required = True)
+	packet_loss = IntField(required = False) # For the moment it is false because i dont know how to get it 
+	timestamp = StringField(required = True)
+	flow_ref = ReferenceField(flow)
+	ip_sla_ref = ReferenceField(ip_sla)
